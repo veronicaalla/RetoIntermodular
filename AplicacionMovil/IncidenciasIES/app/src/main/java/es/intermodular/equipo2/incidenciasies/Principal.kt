4eb44feabb8b171d2dd32e75de11cd7c4d0cfa0f
Vladyslav <73839721+vlady113@users.noreply.github.com>
2024-02-07 01:10:58 +0100

    Login v1.1
    Se ha implementado la lógica para el inicio de sesión. Se ha añadido la traduccion del activity_logging.xml

--- AplicacionMovil/IncidenciasIES/app/src/main/java/es/intermodular/equipo2/incidenciasies/Principal.kt
+++ AplicacionMovil/IncidenciasIES/app/src/main/java/es/intermodular/equipo2/incidenciasies/Principal.kt
@@ -1,8 +1,11 @@
 package es.intermodular.equipo2.incidenciasies
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import es.intermodular.equipo2.incidenciasies.databinding.ActivityPrincipalBinding
+import java.util.Locale
 
 class Principal : AppCompatActivity() {
 
@@ -13,5 +16,18 @@ class Principal : AppCompatActivity() {
         //Inflamos la vista
         binding = ActivityPrincipalBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        fun changeLanguage(view: View) {
+            // Cambiar el idioma de la aplicación aquí
+            // Por ejemplo, para cambiar al inglés:
+            val locale = Locale("en")
+            Locale.setDefault(locale)
+            val config = Configuration()
+            config.locale = locale
+            baseContext.resources.updateConfiguration(config, baseContext.resources.displayMetrics)
+
+            // Reiniciar la actividad para aplicar el cambio de idioma
+            recreate()
+        }
     }
 }
