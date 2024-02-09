0d3f88f3a4140d59e0e2948ef936b960d49ca75f
DESKTOP-2HVSTF7\jdcg1 <josedavid.casasgomez@iesmiguelherrero.com>
2024-02-09 15:40:34 +0100

    Pruebas

--- AplicacionEscritorio/ProyectoIntermodular/ProyectoIntermodular/Formularios/Inicial.cs
+++ AplicacionEscritorio/ProyectoIntermodular/ProyectoIntermodular/Formularios/Inicial.cs
@@ -1,4 +1,5 @@
-﻿using ProyectoIntermodular.Formularios;
+﻿using ProyectoIntermodular.Clases;
+using ProyectoIntermodular.Formularios;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,8 +15,14 @@ namespace ProyectoIntermodular
 {
     public partial class Inicial : Form
     {
+        Perfiles profesor = new Perfiles();
+        Perfiles admin = new Perfiles();
+
         public Inicial()
         {
+            profesor.perfil = PerfilEnum.PROFESOR;
+            admin.perfil = PerfilEnum.ADMIN;
+
             InitializeComponent();
             this.StartPosition = FormStartPosition.CenterScreen;
             pictureBox1.Enabled = false;
@@ -28,9 +35,13 @@ namespace ProyectoIntermodular
             pictureBox1.Show();
             pictureBox1.Enabled = true;
             await Task.Delay(3000);
-            Admin admin = new Admin();
+
+            if (admin.perfil == PerfilEnum.ADMIN) { 
+
+            Admin admin = new Admin(this);
             this.Hide();
             admin.Show();
+            }
         }
         private void Inicial_Load(object sender, EventArgs e)
         {
