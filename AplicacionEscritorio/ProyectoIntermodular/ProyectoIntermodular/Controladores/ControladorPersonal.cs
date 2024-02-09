3d88ddbc9a21a9c02e9d0068cd4723b32543b575
IVÁN\The Shadow <ivan.diazfernandez@iesmiguelherrero.com>
2024-02-09 18:43:24 +0100

    Trato de conectarme a la API

---
+++ AplicacionEscritorio/ProyectoIntermodular/ProyectoIntermodular/Controladores/ControladorPersonal.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using ProyectoIntermodular.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoIntermodular.Controladores
+{
+    internal class ControladorPersonal
+    {
+        private HttpClient client;
+
+        public ControladorPersonal()
+        {
+            client=new HttpClient();
+        } 
+
+        public async Task<Personal> GetPersonal()
+        {
+            try
+            {
+                Personal personal = new Personal();
+                HttpResponseMessage response = await
+                    client.GetAsync("http://localhost:8080/api/personal");
+                response.EnsureSuccessStatusCode();
+
+                string responseJson=await response.Content.ReadAsStringAsync();
+
+                personal=JsonConvert.DeserializeObject<Personal>
+                    (responseJson);
+                return personal;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
--- AplicacionEscritorio/ProyectoIntermodular/ProyectoIntermodular/Formularios/SeleccionarUsuario.cs
+++ AplicacionEscritorio/ProyectoIntermodular/ProyectoIntermodular/Formularios/SeleccionarUsuario.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ProyectoIntermodular.Clases;
+using ProyectoIntermodular.Controladores;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,13 +14,40 @@ namespace ProyectoIntermodular.Formularios
 {
     public partial class SeleccionarUsuario : Form
     {
+        private ControladorPersonal controladorPersonal;
+        private List<Personal> lista;
+        private Personal person;
         public SeleccionarUsuario()
         {
             InitializeComponent();
             this.StartPosition = FormStartPosition.CenterScreen;
+            controladorPersonal= new ControladorPersonal(); 
+            person = new Personal();
+            GetPersonal();
         }
 
-        private void btnVolver_Click(object sender, EventArgs e)
+        private async void GetPersonal()
+        {
+            person = await controladorPersonal.GetPersonal();
+
+            if (person != null)
+            {
+                foreach(var personal in lista)
+                {
+                    DataGridViewRow row = new DataGridViewRow();
+                    row.CreateCells(dgvPerfiles);
+
+                    row.Cells[0].Value = person.nombre;
+                    row.Cells[1].Value = person.apellido1;
+                    row.Cells[2].Value = person.dni;
+                    row.Cells[3].Value = person.direccion;
+
+                    dgvPerfiles.Rows.Add(row);
+                }
+            }
+        }
+
+            private void btnVolver_Click(object sender, EventArgs e)
         {
             ControlUsuarios controlUsuarios = new ControlUsuarios();
             this.Hide();
