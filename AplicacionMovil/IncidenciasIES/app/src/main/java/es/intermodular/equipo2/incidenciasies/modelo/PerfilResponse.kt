2e76d822e73a265cda016d43004f79ba60581ab7
Vladyslav <73839721+vlady113@users.noreply.github.com>
2024-02-11 13:15:15 +0100

    Clases Response
    Se han implementado las clases Response

---
+++ AplicacionMovil/IncidenciasIES/app/src/main/java/es/intermodular/equipo2/incidenciasies/modelo/PerfilResponse.kt
@@ -0,0 +1,38 @@
+package es.intermodular.equipo2.incidenciasies.modelo
+
+import java.io.Serializable
+
+data class PerfilResponse(
+
+    @SerializedName("activo")
+    val activo: Int,
+
+    @SerializedName("apellido1")
+    val apellido1: String,
+
+    @SerializedName("apellido2")
+    val apellido2: String,
+
+    @SerializedName("cp")
+    val cp: Int,
+
+    @SerializedName("direccion")
+    val direccion: String,
+
+    @SerializedName("dni")
+    val dni: String,
+
+    @SerializedName("localidad")
+    val localidad: String,
+
+    @SerializedName("nombre")
+    val nombre: String,
+
+    @SerializedName("tlf")
+    val tlf: String,
+
+    @SerializedName("departamento")
+    val departamento: DepartamentoResponse
+
+
+) : Serializable
