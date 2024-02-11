f558705700bd558f8ddaff9ab09f23f7c6c25b74
Vladyslav <73839721+vlady113@users.noreply.github.com>
2024-02-11 12:33:26 +0100

    Se han refacotirzado elementos de la API
    refactorizado elementos api

---
+++ API/APIRest2_23_24/src/main/java/es/equipo2/apirest1/repository/DepartamentoRepository.java
@@ -0,0 +1,9 @@
+package es.equipo2.apirest1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import es.equipo2.apirest1.model.Departamento;
+
+public interface DepartamentoRepository extends JpaRepository<Departamento, Integer>{
+
+}
--- API/APIRest2_23_24/src/main/java/es/luisherrero/apirest1/model/Tipo_Incidencias.java
+++
@@ -1,5 +0,0 @@
-package es.luisherrero.apirest1.model;
-
-public enum Tipo_Incidencias {
-	EQUIPOS, CUENTAS, WIFI, INTERNET, SOFTWARE
-}
