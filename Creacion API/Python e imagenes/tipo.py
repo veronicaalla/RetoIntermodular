2225affeade12b382fbca5e3f5482b2fbb631ece
vcm5695 <vcm5695@gmail.com>
2024-02-19 18:05:47 +0100

    Add files via upload

---
+++ Creacion API/Python e imagenes/tipo.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class tipo(Enum):
+    EQUIPOS = 1
+    CUENTAS = 2
+    WIFI = 3
+    INTERNET = 4
+    SOFTWARE = 5
---
+++ Creacion API/Python e imagenes/tipoEstado.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class tipoEstado(Enum):
+    Abierta = 1
+    Asignada = 2
+    En_proceso = 3
+    Enviada_a_Infortec = 4
+    Resuelta = 5
+    Cerrada = 6
