4c12352ccdf5d95cdcecfb876e458fecec7a6e5b
AlejandroPenil <119075931+AlejandroPenil@users.noreply.github.com>
2024-02-19 17:43:01 +0100

    Arreglo API

---
+++ API/APIRest2_23_24/src/main/java/es/equipo2/apirest1/model/Tipo_Incidencias.java
@@ -0,0 +1,5 @@
+package es.equipo2.apirest1.model;
+
+public enum Tipo_Incidencias {
+	EQUIPOS, CUENTAS, WIFI, INTERNET, SOFTWARE
+}
---
+++ API/APIRest2_23_24/src/main/java/es/equipo2/apirest1/model/Tipo_Perfil.java
@@ -0,0 +1,5 @@
+package es.equipo2.apirest1.model;
+
+public enum Tipo_Perfil {
+	administrador, profesor, otros
+}
