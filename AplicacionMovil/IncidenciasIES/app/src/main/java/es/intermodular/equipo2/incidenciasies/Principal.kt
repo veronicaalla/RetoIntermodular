313f4c12102a84210ae6211bcfa1f43c9b223cab
Vladyslav <73839721+vlady113@users.noreply.github.com>
2024-02-19 19:43:54 +0100

    Modificaciones conexion API
    Se han realizado varias modificaciones para conectar a la API

--- AplicacionMovil/IncidenciasIES/app/src/main/java/es/intermodular/equipo2/incidenciasies/Principal.kt
+++ AplicacionMovil/IncidenciasIES/app/src/main/java/es/intermodular/equipo2/incidenciasies/Principal.kt
@@ -3,13 +3,14 @@ package es.intermodular.equipo2.incidenciasies
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import es.intermodular.equipo2.incidenciasies.CrearModificarIncidencia.SelectTypeIncidents
 import es.intermodular.equipo2.incidenciasies.databinding.ActivityPrincipalBinding
+import es.intermodular.equipo2.incidenciasies.datos.IncidenciaApi
 import es.intermodular.equipo2.incidenciasies.datos.IncidenciaApiService
 import es.intermodular.equipo2.incidenciasies.datos.RetrofitBuilder
 import es.intermodular.equipo2.incidenciasies.modelo.IncidenciaResponse
@@ -17,7 +18,6 @@ import es.intermodular.equipo2.incidenciasies.recyclerIncidencias.IncidenciaAdap
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import okhttp3.Dispatcher
 import retrofit2.Response
 import retrofit2.Retrofit
 
@@ -124,10 +124,13 @@ class Principal : AppCompatActivity() {
             // Mostrar el menú desplegable
             popupMenu.show()
         }
+
+        retrofit = RetrofitBuilder.build()
         initUI()
     }
 
     private fun initUI() {
+
         //Damos valor al recycler view
         retrofit = RetrofitBuilder.build()
         adapter = IncidenciaAdapter { incidenciaId ->
@@ -141,20 +144,54 @@ class Principal : AppCompatActivity() {
         binding.rvIncidencias.layoutManager = LinearLayoutManager(this)
         binding.rvIncidencias.adapter = adapter
 
-        /*
-        //Mostramos los items
-        CoroutineScope(Dispatchers.IO).launch {
-            val myResponse: Response<List<IncidenciaResponse>> =
-                retrofit.create(IncidenciaApiService::class.java).getIncidencias()
 
-            if (myResponse.isSuccessful) {
-                val listIncidencias: List<IncidenciaResponse>? = myResponse.body()
-                if (listIncidencias != null) {
-                    adapter.setIncidencias(listIncidencias)
+        //Mostramos los items
+        val idUsuarioPrueba: Int = 1;
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val myResponse: Response<List<IncidenciaResponse>> =
+                    retrofit.create(IncidenciaApiService::class.java)
+                        .getIncidenciasUsuario(idUsuarioPrueba)
+
+                if (myResponse.isSuccessful) {
+                    val response: List<IncidenciaResponse>? = myResponse.body()
+                    if (response != null) {
+                        runOnUiThread {
+                            adapter.updateIncidencias(response)
+                        }
+                    }
+                } else {
+                    Log.e("Incidencias", "Error al obtener las incidencias")
                 }
+            } catch (e: Exception) {
+                Log.e("Incidencias", "Error: ${e.message}")
             }
         }
-        */
+
+       /* CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val myResponse: Response<List<IncidenciaResponse>> =
+                    IncidenciaApi.retrofitService.getIncidenciasUsuario(idUsuarioPrueba)
+
+                if (myResponse.isSuccessful) {
+                    Log.i("Patata", "Funciona")
+                    val response: List<IncidenciaResponse>? = myResponse.body()
+                    if (response != null) {
+                        Log.i("Incidencias", response.toString())
+                        runOnUiThread {
+                            adapter.updateIncidencias(response)
+                        }
+                    }
+                } else {
+                    Toast.makeText(applicationContext, "Error al obtener las incidencias", Toast.LENGTH_LONG).show()
+                }
+            } catch (e: Exception) {
+                Log.e("Error", e.message ?: "Error desconocido")
+                runOnUiThread {
+                    Toast.makeText(applicationContext, "Error al obtener las incidencias", Toast.LENGTH_LONG).show()
+                }
+            }
+        }*/
     }
 
     private fun mostrarLayoutAyuda() {
--- AplicacionMovil/IncidenciasIES/app/src/main/java/es/intermodular/equipo2/incidenciasies/recyclerIncidencias/IncidenciaViewHolder.kt
+++ AplicacionMovil/IncidenciasIES/app/src/main/java/es/intermodular/equipo2/incidenciasies/recyclerIncidencias/IncidenciaViewHolder.kt
@@ -5,13 +5,13 @@ import androidx.recyclerview.widget.RecyclerView
 import es.intermodular.equipo2.incidenciasies.databinding.ItemIncidenciasBinding
 import es.intermodular.equipo2.incidenciasies.modelo.IncidenciaResponse
 
-class IncidenciaViewHolder (view:View): RecyclerView.ViewHolder(view){
+class IncidenciaViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     private val binding = ItemIncidenciasBinding.bind(view)
 
-    fun bind (incidenciasResponse:IncidenciaResponse, onItemSelected:(String) -> Unit){
+    fun bind(incidenciasResponse: IncidenciaResponse, onItemSelected: (String) -> Unit) {
         binding.txtFecha.text = incidenciasResponse.fechaCreacion.toString()
-
+        //binding.root.setOnClickListener { onItemSelected(incidenciasResponse.idIncidencia) }
     }
 }
 
