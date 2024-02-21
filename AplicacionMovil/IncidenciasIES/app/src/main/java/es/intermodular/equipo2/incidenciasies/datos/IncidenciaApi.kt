caf4b973ca045d4ea48b5f665e08a76cc84b5724
Vladyslav <73839721+vlady113@users.noreply.github.com>
2024-02-21 18:17:33 +0100

    Botones y listas del principal
    Se ha implementado la funcionalidad para que los botones del principal ataquen a la API y obtengan la informacion para mostrarla en la lista

--- AplicacionMovil/IncidenciasIES/app/src/main/java/es/intermodular/equipo2/incidenciasies/datos/IncidenciaApi.kt
+++
@@ -1,8 +0,0 @@
-package es.intermodular.equipo2.incidenciasies.datos
-
-object IncidenciaApi {
-
-    private val retrofit = RetrofitBuilder.build()
-    val retrofitService: IncidenciaApiService by lazy { retrofit.create(IncidenciaApiService::class.java) }
-
-}
