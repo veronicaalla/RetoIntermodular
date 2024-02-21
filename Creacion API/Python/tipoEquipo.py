d8f93bf4d6f6221c37cf6f2c79efc6c2e5fec32b
vcm5695 <vcm5695@gmail.com>
2024-02-21 18:39:00 +0100

    Add files via upload

---
+++ Creacion API/Python/tipoEquipo.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class tipoEquipo(Enum):
+    Altavoces = 1
+    Impresora = 2
+    Monitor = 3
+    Pantalla_interactiva = 4
+    Portatil_de_aula = 5
+    Portatil_Consejeria = 6
+    Proyector = 7
