364c94bef6b55412a29b24731023275afd2c9421
AlejandroPenil <119075931+AlejandroPenil@users.noreply.github.com>
2024-02-21 17:01:54 +0100

    Resubir la API

---
+++ API/APIRest2_23_24/src/main/java/es/equipo2/apirest1/repository/EquipoRepository.java
@@ -0,0 +1,9 @@
+package es.equipo2.apirest1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import es.equipo2.apirest1.model.Equipo;
+
+public interface EquipoRepository extends JpaRepository<Equipo, Integer>{
+
+}
