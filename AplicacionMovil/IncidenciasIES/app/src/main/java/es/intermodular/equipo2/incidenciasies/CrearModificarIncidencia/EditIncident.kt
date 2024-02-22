6f2b2eb362846b2ede36c7c1f7c68260d84c3ff9
veronicaalla <veronica.alvarezlavin@iesmiguelherrero.es>
2024-02-22 17:22:19 +0100

    Modificaciones para acceder a una misma clase de diferentes formas y cada forma, realice una accion diferentes --> Las acciones estan por codificar

--- AplicacionMovil/IncidenciasIES/app/src/main/java/es/intermodular/equipo2/incidenciasies/CrearModificarIncidencia/EditIncident.kt
+++ AplicacionMovil/IncidenciasIES/app/src/main/java/es/intermodular/equipo2/incidenciasies/CrearModificarIncidencia/EditIncident.kt
@@ -11,43 +11,45 @@ import java.time.format.DateTimeFormatter
 
 class EditIncident : AppCompatActivity() {
 
-    private lateinit var incidencia:IncidenciaResponse
-
     companion object {
         const val EXTRA_EDIT_INCIDENCIA = "extra_incidencia_index"
         const val EXTRA_EDITED_INCIDENCIA = "extra_edited_incidencia"
+        const val TIPO_INCIDENCIA: String = ""
         private const val REQUEST_CODE_EDIT = 1
     }
+
     private lateinit var binding: ActivityEditIncidentBinding
 
     //Le pasamos los parametros de el tipo de incidencia
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_edit_incident)
 
         binding = ActivityEditIncidentBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         //Recogemos la incidencia en caso de haber seleccionado una
-        incidencia = intent.getSerializableExtra(EXTRA_EDIT_INCIDENCIA) as IncidenciaResponse
-        Log.i("Incidencia a editar", incidencia.toString())
-        if (incidencia==null){
 
-            //Recogemos el tipo de incidencia que se ha pasado mediante un Intent
-            val tipo = intent.extras?.getString("tipo").orEmpty()
-            binding.txtTipoIncidencia.text = tipo
+        var tipoIncidencia = intent.getStringExtra("tipo").toString()
 
-            //El campo -> Fecha de creacion se autocompleta con la fecha actual siempre y cuando el ID incidencia sea 0
-            val actuallyDate = LocalDateTime.now()
-                .format(DateTimeFormatter.ofPattern("MMM dd yyyy"))
-
-            binding.editTextFechaCreacion.setText(actuallyDate)
+        if (tipoIncidencia != "") {
+            crearIncidencia(tipoIncidencia)
+        } else {
+            //SI el tipo de incidencia esta vacio, significa que se le ha pasado directamente la incidencia
+            var incidencia =
+                intent.getSerializableExtra(EXTRA_EDIT_INCIDENCIA) as IncidenciaResponse
+            Log.i("Incidencia a editar", incidencia.toString())
+            modificarIncidencia(incidencia)
         }
 
 
+    }
 
+    private fun crearIncidencia(tipoIncidencia: String) {
 
+    }
+
+    private fun modificarIncidencia(incidencia: IncidenciaResponse) {
 
     }
 }
